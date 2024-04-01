98712451b77c306959e5c9821c65f5d76f044f70
Paco Cordobes <ghiret@gmail.com>
2024-04-01 18:46:25 +0000

    Add new files, update navigation, and fix imports

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,19 +2,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = (props) => (
-  <nav className='column is-2 menu'>
+  <nav className="column is-2 menu">
     <p className="menu-label">Menu</p>
     <ul className="menu-list">
-      <li>
-        <NavLink to='/products' className={({ isActive }) => isActive ? 'active-link' : undefined}>
-          Products
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to='/about' className={({ isActive }) => isActive ? 'active-link' : undefined}>
-          About
-        </NavLink>
-      </li>
+      <NavLink to="/products" activeClassName="active-link">
+        Products
+      </NavLink>
+      <NavLink to="/about" activeClassName="active-link">
+        About
+      </NavLink>
     </ul>
     {props.children}
   </nav>
